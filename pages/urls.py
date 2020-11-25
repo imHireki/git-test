10f93e080775f8a11613f744e0e2c61c4d004b2f
Hireki <imhireki@gmail.com>
2020-11-25 11:18:30 -0300

    forms testing

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('simbol-emoji/', views.simbolemoji, name='simbolemoji'),
     path('text-convert/', views.textconvert, name='textconvert'),
+    path('your-name/', views.get_name, name='your_name'),
+    path('thanks/', views.get_name, name='your_name'),
 ]
